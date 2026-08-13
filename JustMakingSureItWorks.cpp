c8e4ff3d284d75d3f919dce3eb38d4885d54c99f
AlonsoBP-cell <aborquezp15@gmail.com>
2026-08-13 19:00:46 -0400

    jarona

---
+++ JustMakingSureItWorks.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+
+    cout << "Whats your name?" << endl;
+    cin >> name;
+
+    cout << name << ", welcome to C++ programming!" << endl;
+    return 0;
+
+}
